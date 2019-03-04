bcbaee96cd560a791bb4caddbf81a3bbf11f7ce4
raja <rajasekar3eg@gmail.com>
2019-03-04 18:02:41 +0530

    modularised and gave proper name

--- src/bin/aco_using_memmap.rs
+++ src/bin/aco_using_memmap.rs
@@ -15,7 +15,7 @@ use std::io::BufWriter;
 use std::mem::transmute;
 use std::{mem, slice};
 
-fn main5() {
+fn main() {
     println!("Hello, world!");
     let f = File::open("/home/datascience/Documents/aco_rust/cities.csv").unwrap();
     let file = BufReader::new(&f);
@@ -229,44 +229,3 @@ fn main1() {
     //println!("{:?}", distance);
 }
 
-fn main() {
-    println!("Hello, world!");
-    // let f = File::open("/home/datascience/Documents/aco_rust/cities.csv").unwrap();
-    let f = File::open("cities.csv").unwrap();
-    let file = BufReader::new(&f);
-    let mut distance: Vec<aco_no_mem::Coordinate> = Vec::new();
-    for line in file.lines() {
-        let line = line.unwrap();
-        let line = line.trim();
-        let line: Vec<&str> = line.split(",").collect();
-        distance.push(aco_no_mem::Coordinate {
-            x: line[1].parse().unwrap(),
-            y: line[2].parse().unwrap(),
-        });
-    }
-    let f = File::open("submission.csv").unwrap();
-    let file = BufReader::new(&f);
-    let mut initial_tour: Vec<i32> = Vec::new();
-    for line in file.lines() {
-        let line = line.unwrap();
-        let line = line.trim();
-        initial_tour.push(line.parse().unwrap());
-    }
-
-    let mut antcolony = aco_no_mem::AntColony {
-        no_ants: 10,
-        no_best_paths: 100,
-        no_iterations: 100,
-        decay: None,
-        alpha: 1.0,
-        beta: 1.0,
-        distances: distance,
-        // initial_tour: None,
-        initial_tour: Some(initial_tour),
-        default_pheromone: 0.0,
-        nodes: Vec::new(),
-        pheromones: FnvHashMap::default(),
-        //pheromones: HashMap::default(),
-    };
-    println!("{:?}", antcolony.run());
-}
